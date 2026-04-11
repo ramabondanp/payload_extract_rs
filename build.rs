b93eaece0d12408814e630734875df8670118384
kmiit <kmiit@foxmail.com>
2026-04-11 16:58:08 +0800

    Use protoc-bin-vendored to handle protoc (#1)

--- build.rs
+++ build.rs
@@ -1,10 +1,15 @@
-fn main() -> std::io::Result<()> {
-    prost_build::Config::new().compile_protos(
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+
+    let mut config = prost_build::Config::new();
+    config.protoc_executable(protoc);
+    config.compile_protos(
         &[
             "src/proto/update_metadata.proto",
             "src/proto/lz4diff.proto",
         ],
         &["src/proto/"],
     )?;
+
     Ok(())
 }
