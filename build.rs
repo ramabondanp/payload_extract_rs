6a9c05beabfed404a86f34928cbb4f9cce3c5053
kmiit <kmiit@foxmail.com>
2026-04-26 00:16:00 +0800

    build: Use default protoc path on android host (#2)

--- build.rs
+++ build.rs
@@ -1,8 +1,18 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let protoc = protoc_bin_vendored::protoc_bin_path()?;
-
     let mut config = prost_build::Config::new();
-    config.protoc_executable(protoc);
+
+     match protoc_bin_vendored::protoc_bin_path() {
+         Ok(protoc) => {
+             config.protoc_executable(protoc);
+         }
+         Err(err) => {
+             println!(
+                 "cargo:warning=Unable to use vendored protoc ({}); falling back to PROTOC or `protoc` on PATH",
+                 err
+             );
+         }
+     }
+
     config.compile_protos(
         &["src/proto/update_metadata.proto", "src/proto/lz4diff.proto"],
         &["src/proto/"],
